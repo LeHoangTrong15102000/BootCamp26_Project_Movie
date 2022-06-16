fcad8a2378e33fed522d458eb7c175065c123062
Le Hoang Trong <langtupro0456@gmail.com>
2022-06-16 22:05:25 +0700

    comment booking component

--- src/modules/Checkout/pages/Booking.jsx
+++ src/modules/Checkout/pages/Booking.jsx
@@ -3,8 +3,11 @@
 // Khách hàng phải đăng nhập thì mới cho vào trang này, còn không thì redirect họ về trang đăng nhập
 import React from 'react';
 
+
+// Lưu thông tin của trang booking với cái id của bộ phim ngay phía sau /login để khi đăng nhập thành công thì sẽ navigate họ về dùng trang đang đứng hồi nảy
 const Booking = (props) => {
   return <div>Booking Page</div>;
 };
 
 export default Booking;
+  
