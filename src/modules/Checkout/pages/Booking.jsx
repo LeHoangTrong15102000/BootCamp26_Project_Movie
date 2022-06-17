cc9cb9e6516dacac2e2e7b3733eceb4a87780fc8
Le Hoang Trong <langtupro0456@gmail.com>
2022-06-17 11:07:04 +0700

    comment code booking

--- src/modules/Checkout/pages/Booking.jsx
+++ src/modules/Checkout/pages/Booking.jsx
@@ -3,11 +3,10 @@
 // Khách hàng phải đăng nhập thì mới cho vào trang này, còn không thì redirect họ về trang đăng nhập
 import React from 'react';
 
-
-// Lưu thông tin của trang booking với cái id của bộ phim ngay phía sau /login để khi đăng nhập thành công thì sẽ navigate họ về dùng trang đang đứng hồi nảy
+// Lưu thông tin của trang booking với cái id của bộ phim ngay phía sau /login để khi đăng nhập thành công thì sẽ navigate họ về dùng trang đang đứng hồi nảy(với cái searchUrl có id của bộ phim hồi nảy).
+// sau đó navigate trực tiếp vào trang đang đứng trước khi bị đưa vào trang login
 const Booking = (props) => {
   return <div>Booking Page</div>;
 };
 
 export default Booking;
-  
