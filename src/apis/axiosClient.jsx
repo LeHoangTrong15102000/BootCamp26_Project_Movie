2afdcfa660cfecb2824b8fd9c6e56a0981b02fe5
Le Hoang Trong <langtupro0456@gmail.com>
2022-06-30 15:42:28 +0700

    add comment con

--- src/apis/axiosClient.jsx
+++ src/apis/axiosClient.jsx
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import qs from 'query-string';
+import axios from "axios";
+import qs from "query-string";
 
 // setup những cấu hình mặc định cho axios
 const axiosClient = axios.create({
-  baseURL: 'https://movienew.cybersoft.edu.vn/api/',
+  baseURL: "https://movienew.cybersoft.edu.vn/api/",
   headers: {
     TokenCybersoft:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNiIsIkhldEhhblN0cmluZyI6IjEzLzEwLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2NTYxOTIwMDAwMCIsIm5iZiI6MTYzMzE5NDAwMCwiZXhwIjoxNjY1NzY2ODAwfQ.TMg-RWGpT6_kH-eG3Pbw5j_8yWUP84LrkWZAFj-Drfk',
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAyNiIsIkhldEhhblN0cmluZyI6IjEzLzEwLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2NTYxOTIwMDAwMCIsIm5iZiI6MTYzMzE5NDAwMCwiZXhwIjoxNjY1NzY2ODAwfQ.TMg-RWGpT6_kH-eG3Pbw5j_8yWUP84LrkWZAFj-Drfk",
   },
 
   // override lại cách axios set params lên URL
