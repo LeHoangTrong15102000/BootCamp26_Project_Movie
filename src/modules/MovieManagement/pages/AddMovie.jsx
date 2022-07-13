3a16a5079a23dd6f984742b44972a27c548d9c78
Le Hoang Trong <langtupro0456@gmail.com>
2022-07-13 20:59:40 +0700

    quản lí thêm phim

--- src/modules/MovieManagement/pages/AddMovie.jsx
+++ src/modules/MovieManagement/pages/AddMovie.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const AddMovie = () => {
+
+  const {} = useForm({
+    defaultValues: {
+      tenPhim: "",
+      trailer: "",
+      moTa: "",
+      maNhom: "GP01",
+      ngayKhoiChieu: "",
+      sapChieu: "",
+      dangChieu: "",
+      hot: "",
+      danhGia: "",      
+    }
+  })
   return (
-    <div>AddMovie</div>
+    <form></form>
   )
 }
 
